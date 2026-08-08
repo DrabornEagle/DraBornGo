501603ba84508eedd7bb77cb0098bb6696d82093
DrabornEagle <draborneagle@gmail.com>
2026-08-08 16:16:29 +0300

    chore: align v0.0.8 Android policy config

--- app.config.js
+++ app.config.js
@@ -14,6 +14,7 @@ function dkd_android_permission_value(dkd_permission_suffix_parts_value) {
 const dkd_app_json_value = dkd_read_json_file('app.json');
 const dkd_package_json_value = dkd_read_json_file('package.json');
 const dkd_android_target_sdk_value = 36;
+const dkd_android_version_code_value = 8;
 
 let dkd_public_env_defaults_value = {};
 
@@ -83,7 +84,7 @@ function dkd_clean_android_config_value(dkd_source_android_config_value = {}) {
 
   return {
     ...dkd_clean_config_value,
-    versionCode: dkd_source_android_config_value.versionCode || 7,
+    versionCode: dkd_source_android_config_value.versionCode || dkd_android_version_code_value,
     permissions: dkd_safe_android_permissions_value.filter(
       (dkd_permission_name_value) => !dkd_blocked_android_permission_set_value.has(dkd_permission_name_value)
     )
@@ -118,7 +119,7 @@ module.exports = function dkd_resolve_expo_app_config(dkd_config_params_value) {
     android: {
       ...dkd_base_android_config_value,
       ...dkd_project_android_config_value,
-      versionCode: dkd_android_config_value.versionCode || 7,
+      versionCode: dkd_android_config_value.versionCode || dkd_android_version_code_value,
       permissions: dkd_safe_android_permissions_value
     },
     plugins: [
@@ -129,6 +130,7 @@ module.exports = function dkd_resolve_expo_app_config(dkd_config_params_value) {
       ...(dkd_base_config_value.extra || {}),
       ...(dkd_expo_config_value.extra || {}),
       dkd_android_target_sdk: dkd_android_target_sdk_value,
+      dkd_android_version_code: dkd_android_version_code_value,
       dkd_public_env: dkd_public_env_config_value,
       EXPO_PUBLIC_SUPABASE_URL: dkd_public_env_config_value.EXPO_PUBLIC_SUPABASE_URL,
       EXPO_PUBLIC_SUPABASE_ANON_KEY: dkd_public_env_config_value.EXPO_PUBLIC_SUPABASE_ANON_KEY,
