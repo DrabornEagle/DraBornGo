e4838d335a5cd82c7491de77a59cceaff9402433
DrabornEagle <draborneagle@gmail.com>
2026-08-11 00:23:44 +0300

    fix: enable courier board v4

--- src/core/ModalHost.js
+++ src/core/ModalHost.js
@@ -1,7 +1,7 @@
 import React, { memo, useCallback, useState } from 'react';
 import ActionMenuModal from '../features/navigation/ActionMenuModal';
 import ProfileModal from '../features/profile/ProfileModal';
-import CourierBoardModal from '../features/courier/dkd_courier_board_modal_v3';
+import CourierBoardModal from '../features/courier/dkd_courier_board_modal_v4';
 import DkdCourierLiveSyncBridge from '../features/courier/dkd_courier_live_sync_bridge';
 import DkdLiveSupportModal from '../features/support/dkd_live_support_modal_v2';
 import AdminMenuModal from '../features/admin/AdminMenuModal';
